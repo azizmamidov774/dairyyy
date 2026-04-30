458a2b7427cb799b50c0925b13744eaa0f325466
mamidov <abd200888002@gmail.com>
2026-04-30 18:03:09 +0600

    first commit

--- auth/settings.py
+++ auth/settings.py
@@ -29,7 +29,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_HEADERS = ["*"]
+
+CORS_ALLOW_METHODS = ["GET", "POST", "OPTIONS", "PUT", "DELETE"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -61,17 +65,19 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
 ROOT_URLCONF = 'auth.urls'
 
 TEMPLATES = [
